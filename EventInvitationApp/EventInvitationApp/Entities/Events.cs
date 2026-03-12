6d8aca1132da1935c63aacd87ab1b4bcfbf8d9f5
muhdikhwan01 <muhammadikhwan123.mi@gmail.com>
2026-03-12 11:05:33 +0800

    cleanup: remove unrelated project folders

--- EventInvitationApp/EventInvitationApp/Entities/Events.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EventInvitationApp.Entities
-{
-    public class Events
-    {
-
-    }
-}
-
-//must have virtual keyword
--- EventInvitationApp/EventInvitationApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EventInvitationApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
