e2e788e545aa1ddb06460b067cac1c50867284a0
muhdikhwan01 <muhammadikhwan123.mi@gmail.com>
2026-05-29 17:20:39 +0800

    feat: establish clean architecture foundation and EF Core setup

---
+++ Pesutima/Pesutima.Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Pesutima.Domain.Entities;
+
+namespace Pesutima.Persistence.Context;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Member> Members { get; set; }
+}
---
+++ Pesutima/Pesutima.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pesutima.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
